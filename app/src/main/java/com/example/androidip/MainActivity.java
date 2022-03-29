cbccae2cbc0ace5b64daf809071a088c9e53bf5b
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-29 14:33:32 +0300

    Edit layouts

--- app/src/main/java/com/example/androidip/MainActivity.java
+++ app/src/main/java/com/example/androidip/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.androidip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidip.databinding.ActivitySearchBinding;
 
 public class MainActivity extends AppCompatActivity {
+     private ActivitySearchBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+//        setContentView(R.layout.activity_main);
     }
 }
