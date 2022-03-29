14b0ac5f31b7c9fedf18be858d4c722794ded019
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-29 17:05:34 +0300

    Edit Splash Screen

--- app/src/main/java/com/example/androidip/SplashActivity.java
+++ app/src/main/java/com/example/androidip/SplashActivity.java
@@ -2,13 +2,38 @@ package com.example.androidip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.androidip.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //Binding Class
+    private ActivitySplashBinding activitySplashBinding;
+
+    //Splash Screen
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    //After completion of 2 seconds, the next activity will get started.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //This method is used so that your splash activity can cover the entire screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Get next Layout
+        activitySplashBinding = ActivitySplashBinding.inflate(getLayoutInflater());
+        View view = activitySplashBinding.getRoot();
+        setContentView(view);
+
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
+
     }
 }
