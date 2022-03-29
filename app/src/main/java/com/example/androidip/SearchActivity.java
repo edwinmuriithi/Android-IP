e2c3640a2d144d4f95bb8ee6701f2adbc6ed9b05
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-29 15:02:39 +0300

    Set onClick on Search page

--- app/src/main/java/com/example/androidip/SearchActivity.java
+++ app/src/main/java/com/example/androidip/SearchActivity.java
@@ -1,4 +1,33 @@
 package com.example.androidip;
 
-public class SearchActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidip.databinding.ActivitySearchBinding;
+
+public class SearchActivity extends AppCompatActivity {
+
+    //Binding Class
+    private ActivitySearchBinding activitySearchBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Get layout
+        activitySearchBinding = ActivitySearchBinding.inflate(getLayoutInflater());
+        View view = activitySearchBinding.getRoot();
+        setContentView(view);
+
+        //Search button Toast
+        activitySearchBinding.search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SearchActivity.this, "Searching...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 }
