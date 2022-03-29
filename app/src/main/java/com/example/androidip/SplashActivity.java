295b0ddb586ad39f08a5a2a9f9b96e61ebe25b1d
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-29 17:44:31 +0300

    Edit Splash Screen

--- app/src/main/java/com/example/androidip/SplashActivity.java
+++ app/src/main/java/com/example/androidip/SplashActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.window.SplashScreen;
 
 import com.example.androidip.databinding.ActivitySplashBinding;
 
@@ -14,26 +16,23 @@ public class SplashActivity extends AppCompatActivity {
     //Binding Class
     private ActivitySplashBinding activitySplashBinding;
 
-    //Splash Screen
-    private static int SPLASH_SCREEN_TIME_OUT=2000;
-    //After completion of 2 seconds, the next activity will get started.
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //This method is used so that your splash activity can cover the entire screen.
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        //Get next Layout
+        //Get Layout
         activitySplashBinding = ActivitySplashBinding.inflate(getLayoutInflater());
         View view = activitySplashBinding.getRoot();
         setContentView(view);
-
-        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-
+       //Splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Go to next layout
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);//Page delays for 2 seconds
 
     }
 }
