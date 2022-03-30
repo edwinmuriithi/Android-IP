213482dc072c23cb199dce1925598dd079774ba8
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-30 18:04:50 +0300

    Edit Recycler Adaper

--- app/src/main/java/com/example/androidip/MainActivity.java
+++ app/src/main/java/com/example/androidip/MainActivity.java
@@ -9,10 +9,15 @@ import android.view.View;
 import com.example.androidip.databinding.ActivityMainBinding;
 import com.example.androidip.databinding.ActivitySearchBinding;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     //Binding Class
      private ActivityMainBinding activityMainBinding;
+     RecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
         View view = activityMainBinding.getRoot();
         setContentView(view);
 
-
-    activityMainBinding.searchagain.setOnClickListener(new View.OnClickListener() {
+        //Onclick
+        activityMainBinding.searchagain.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             //Go to next Layout
@@ -31,6 +36,21 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     });
+        //recycler Constructor
+        recyclerAdapter = new RecyclerAdapter();
+
+        activityMainBinding.recyclerView.setAdapter(recyclerAdapter);
+        initDiseaseName();//Calling the method
 
     }
+    private void initDiseaseName(){
+        List<String> diseaseName = new ArrayList<>();
+        diseaseName.add("Fever");
+        diseaseName.add("Flu");
+        diseaseName.add("Shaking");
+        diseaseName.add("Headache");
+        diseaseName.add("Tiredness");
+        recyclerAdapter.updateDiseaseName(diseaseName);
+    }
+
 }
--- app/src/main/java/com/example/androidip/RecyclerAdapter.java
+++ app/src/main/java/com/example/androidip/RecyclerAdapter.java
@@ -1,4 +1,60 @@
 package com.example.androidip;
 
-public class RecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidip.databinding.RecyclerRowBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    List<String> diseaseName = new ArrayList<>();
+
+    //method to update disease names
+    public void updateDiseaseName(List<String> diseaseName){
+        this.diseaseName = diseaseName;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(recyclerRowBinding);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindView(diseaseName.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return diseaseName.size();//size of disease name
+    }
+
+    //View holder
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        //Constructor for the ViewHolder
+        private RecyclerRowBinding recyclerRowBinding;
+
+        public ViewHolder(@NonNull RecyclerRowBinding recyclerRowBinding){
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+        }
+        //Bind object to recycler view
+        public void bindView(String diseaseName){
+            recyclerRowBinding.diseaseTextView.setText(diseaseName);
+        }
+
+    }
 }
