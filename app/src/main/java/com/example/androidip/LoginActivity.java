8dee11cffd6e263a1e54869d408aeff194bfe5af
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-12 05:13:36 +0300

    Edit Login Page

--- app/src/main/java/com/example/androidip/LoginActivity.java
+++ app/src/main/java/com/example/androidip/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.androidip;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidip.databinding.ActivityLoginBinding;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -66,6 +69,8 @@ public class LoginActivity extends AppCompatActivity {
                 .createSignInIntentBuilder()
                 .setAvailableProviders(provider)
                 .setTosAndPrivacyPolicyUrls("", "")
+                .setLogo(R.drawable.logo)
+                .setAlwaysShowSignInMethodScreen(true)
                 .build();
 
                 startActivityForResult(intent, AUTHUI_REQUEST_CODE);
@@ -75,7 +80,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == AUTHUI_REQUEST_CODE){
+            if (requestCode == RESULT_OK){
+                //We have signed in or have a new user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: " + user.getEmail());
+                    if (user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()){
+                        Toast.makeText(this, "Welcome new User", Toast.LENGTH_SHORT).show();
+                    }else {
+                        //This is a returning user
+                        Toast.makeText(this, "Welcome Back Again", Toast.LENGTH_SHORT).show();
+                    }
+                Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent)
+                    this.finish();
 
+            } else {
+                //sign in failed
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                
+                if (response == null){
+                    Log.d(TAG, "onActivityResult:  the user has cancelled sign in request");
+                } else {
+                    Log.e(TAG, "onActivityResult: ", response.getError() );
+                }
+            }
         }
     }
 }
