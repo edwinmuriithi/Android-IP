c1d79e5e79b23bedf33013028d6f314750816c7a
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-04-20 22:35:24 +0300

    Edit Splash animation

--- app/src/main/java/com/example/androidip/SplashActivity.java
+++ app/src/main/java/com/example/androidip/SplashActivity.java
@@ -7,12 +7,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.window.SplashScreen;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.androidip.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
+    TextView appname;
+    LottieAnimationView lottie;
     //Binding Class
     private ActivitySplashBinding activitySplashBinding;
 
@@ -24,15 +28,19 @@ public class SplashActivity extends AppCompatActivity {
         View view = activitySplashBinding.getRoot();
         setContentView(view);
        //Splash screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        activitySplashBinding.appname.animate().translationY(2000).setDuration(1000).setStartDelay(5000);
+        activitySplashBinding.lottie.animate().translationY(1500).setDuration(1000).setStartDelay(5000);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //Go to next layout
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();//End Splash Activity 
+
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+
             }
-        }, 2000);//Page delays for 2 seconds
+        },6000);
 
     }
 }
